8d9e7dc5e466176722b893b3bf93d1cdede7fe52
matikitli <m.kitlinos@gmail.com>
2024-12-17 22:09:31 +0000

    x

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   images: {
     domains: [
       'dogeedog.s3.eu-west-2.amazonaws.com',
-      `${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com`
+      `${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+      'lh3.googleusercontent.com'
     ]
   },
   // Only enable React strict mode in development
